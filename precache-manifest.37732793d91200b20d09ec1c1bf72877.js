c03fe9f3c404cc7c90fc8603c4036f81cfc5dfba
Jeriah Bowers <jeriah97bowers@yahoo.com>
2020-04-03 00:35:06 -0400

    Updates

---
+++ precache-manifest.37732793d91200b20d09ec1c1bf72877.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7ca383e7a37a0c7745013c928755447",
+    "url": "/trivia360-game/index.html"
+  },
+  {
+    "revision": "7fd23bdb1325003c5eed",
+    "url": "/trivia360-game/static/css/main.58b45a7d.chunk.css"
+  },
+  {
+    "revision": "7bf4a5d28dbe00c27439",
+    "url": "/trivia360-game/static/js/2.02553c24.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/trivia360-game/static/js/2.02553c24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fd23bdb1325003c5eed",
+    "url": "/trivia360-game/static/js/main.968c0554.chunk.js"
+  },
+  {
+    "revision": "3b5913ce64714da58aba",
+    "url": "/trivia360-game/static/js/runtime-main.c27d69a4.js"
+  }
+]);
